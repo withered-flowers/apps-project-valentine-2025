0b88d39f53767d5d8821fec3823dee7682c3a7d1
Withered Flowers <withered-flowers@users.noreply.github.com>
2025-01-22 08:58:35 +0700

    feat: prepare for astro db

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,8 +3,11 @@ import { z } from "astro:schema";
 
 export const server = {
 	formHandler: defineAction({
+		accept: "form",
 		input: z.undefined(),
 		handler: async () => {
+			// TODO: Implement save to drizzle turso db
+
 			return {
 				message: "Hello from the server!",
 			};
