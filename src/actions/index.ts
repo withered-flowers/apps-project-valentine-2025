83f921585a0badc2a2e7efe21eb61af4b5e5d2fc
Withered Flowers <withered-flowers@users.noreply.github.com>
2025-02-03 12:58:21 +0700

    feat: form completed

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,13 +3,21 @@ import { z } from "astro:schema";
 
 export const server = {
 	formHandler: defineAction({
-		accept: "form",
-		input: z.object({}),
-		handler: async () => {
+		// accept: "form",
+		accept: "json",
+		input: z.object({
+			"your-name": z
+				.string()
+				.min(1, { message: "Please enter your name" }),
+			"so-name": z.string().min(1, {
+				message: "Please enter your Significant Other's name",
+			}),
+		}),
+		handler: async (input) => {
 			// TODO: Implement save to drizzle turso db
 
 			return {
-				message: "Hello from the server!",
+				url: "/result",
 			};
 		},
 	}),
