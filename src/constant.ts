30bc53cab0d989bc3d99e08d1e720e4c2236a912
Withered Flowers <withered-flowers@users.noreply.github.com>
2025-02-04 17:32:43 +0700

    feat: adding animation letter message

--- src/constant.ts
+++ src/constant.ts
@@ -1,1 +1,43 @@
 export const GITHUB_REPO_URL = "https://github.com/withered-flowers/";
+export const MESSAGE_TO_SHOW = [
+  {
+    say: "I Love You",
+    origin_language: "English",
+  },
+  {
+    say: "Je t'aime",
+    origin_language: "French",
+  },
+  {
+    say: "Ti amo",
+    origin_language: "Italian",
+  },
+  {
+    say: "Te amo",
+    origin_language: "Spanish",
+  },
+  {
+    say: "Ich liebe dich",
+    origin_language: "German",
+  },
+  {
+    say: "愛してる",
+    origin_language: "Japanese",
+  },
+  {
+    say: "사랑해",
+    origin_language: "Korean",
+  },
+  {
+    say: "我爱你",
+    origin_language: "Chinese (Mandarin)",
+  },
+  {
+    say: "Ek het jou lief",
+    origin_language: "Afrikaans",
+  },
+  {
+    say: "Jeg elsker dig",
+    origin_language: "Danish",
+  }
+];
