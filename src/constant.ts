ee33c65a0830b58a59fa3bc50bd27a02cf1fec79
Withered Flowers <withered-flowers@users.noreply.github.com>
2025-02-05 11:39:56 +0700

    feat: change message

--- src/constant.ts
+++ src/constant.ts
@@ -1,43 +1,53 @@
 export const GITHUB_REPO_URL = "https://github.com/withered-flowers/";
 export const MESSAGE_TO_SHOW = [
   {
-    say: "I Love You",
-    origin_language: "English",
-  },
-  {
-    say: "Je t'aime",
-    origin_language: "French",
-  },
-  {
-    say: "Ti amo",
-    origin_language: "Italian",
-  },
-  {
-    say: "Ich liebe dich",
-    origin_language: "German",
-  },
-  {
-    say: "Te amo",
-    origin_language: "Spanish",
-  },
-  {
-    say: "愛してる",
-    origin_language: "Japanese",
-  },
-  {
-    say: "사랑해 (Saranghae)",
+    say: "내 발렌타인이 되어 줄래?",
+    pronounciation: "nae ballentain-i doeeo jullae?",
     origin_language: "Korean",
   },
   {
-    say: "我爱你 (wǒ ài nǐ)",
+    say: "Will you be my valentine?",
+    pronounciation: undefined,
+    origin_language: "English",
+  },
+  {
+    say: "Veux-tu être mon/ma valentine?",
+    pronounciation: undefined,
+    origin_language: "French",
+  },
+  {
+    say: "Vuoi essere il mio valentino/la mia valentina?",
+    pronounciation: undefined,
+    origin_language: "Italian",
+  },
+  {
+    say: "Willst du mein Valentinsschatz sein?",
+    pronounciation: undefined,
+    origin_language: "German",
+  },
+  {
+    say: "¿Quieres ser mi San Valentín?",
+    pronounciation: undefined,
+    origin_language: "Spanish",
+  },
+  {
+    say: "バレンタインになってくれませんか？",
+    pronounciation: "Barentain ni natte kuremasen ka?",
+    origin_language: "Japanese",
+  },
+  {
+    say: "做我的情人好吗",
+    pronounciation: "zuò wǒ de qíng rén hǎo ma?",
     origin_language: "Chinese (Mandarin)",
   },
   {
-    say: "Ek het jou lief",
+    say: "Wil jy my valentine wees?",
+    pronounciation: undefined,
     origin_language: "Afrikaans",
   },
   {
-    say: "Jeg elsker dig",
+    say: "Vil du være min valentine?",
+    pronounciation: undefined,
     origin_language: "Danish",
   }
 ];
